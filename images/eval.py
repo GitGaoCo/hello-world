3b9922d50c7d5070b39d9227e89cffbc34c9a37e
Gary Gao <gao.yng@gmail.com>
2019-09-06 10:26:35 -0700

    version A

---
+++ images/eval.py
@@ -0,0 +1,7 @@
+
+##### 
+#This is how to convert from string exppression to equation
+#it is very simple just use eval() function and python will return numerical result
+
+
+print eval('- (3 + ( 2 - 1 ) )')
